15f697ad0526c464f97a65b6e20da54b627c2c74
yenpangchen <86139772+yenpangchen@users.noreply.github.com>
2021-08-02 19:18:53 +0800

    Update basketball_app.py

--- basketball_app.py
+++ basketball_app.py
@@ -8,21 +8,21 @@ st.set_page_config(layout="wide")
 
 # 選擇例行賽還是季後賽
 option = ["例行賽", "季後挑戰賽"]
-seasons=st.sidebar.selectbox("例行賽",option)
+seasontype=st.sidebar.selectbox("例行賽",option)
 
-# 讀取 CSV 資料
+# 爬蟲抓資料
 @st.cache
-def getData(seasons):
-    if(seasons == option[0]):
+def getData(seasontype):
+    if(seasontype == option[0]):
         playerstats=pd.read_csv("regularseason.csv")
-    if(seasons == option[1]):
+    if(seasontype == option[1]):
         playerstats=pd.read_csv("playoffs.csv")
     return playerstats
 
-playerstats = getData(seasons)
+playerstats = getData(seasontype)
 
 # 標題
-if(seasons == option[0]):
+if(seasontype == option[0]):
     st.markdown("""
     # P.League+ Players Stats 
     (Regular Season)
@@ -47,12 +47,21 @@ selected_order=st.sidebar.selectbox("排序方式", sorted_order)
 # 球員姓名
 title = st.sidebar.text_input('搜尋球員姓名')
 
+# 更正三分%因為是 str 所以會有排序錯誤的問題
+def sort_values_by_selected_order(df, selected_order):
+    df=df.replace('%',"",regex=True)
+    df[["兩分%", "三分%", "罰球%"]]=df[["兩分%", "三分%", "罰球%"]].astype('float64')
+    df=df.sort_values(by=selected_order, ascending=False)
+    df[["兩分%", "三分%", "罰球%"]]=df[["兩分%", "三分%", "罰球%"]].astype('str')
+    df[["兩分%", "三分%", "罰球%"]]=df[["兩分%", "三分%", "罰球%"]]+"%"
+    return df
+
 # filtering data
 
 df_selected_team_and_order = playerstats[(playerstats["球隊"].isin(selected_teams))]
 st.header('Display Player Stats of Selected Team(s) & Selected Order')
 st.write('Data Dimension: ' + str(df_selected_team_and_order.shape[0]) + ' rows and ' + str(df_selected_team_and_order.shape[1]) + ' columns.')
-st.dataframe(df_selected_team_and_order.sort_values(by=selected_order, ascending=False))
+st.dataframe(sort_values_by_selected_order(df_selected_team_and_order, selected_order))
 
 names = pd.Series(title)
 names = sorted(names)
@@ -64,3 +73,4 @@ if len(title)!=0:
         st.dataframe(df)
     else:
         st.write("Players Not Found")
+
